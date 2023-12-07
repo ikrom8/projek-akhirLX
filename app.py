669e239b0c172169b9e6a52c277a756678f2432d
unknown <nurikrom948@gmail.com>
2023-12-07 22:54:40 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import (
+    Flask,
+    render_template,
+    url_for
+)
+
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__== '__main__':
+    app.run('0.0.0.0',port=5000, debug=True)
