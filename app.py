4a46e4225331dd55118a8dafcb562c600d9a1fc5
unknown <nurikrom948@gmail.com>
2023-12-12 11:14:06 +0700

    12/12/23

--- app.py
+++ app.py
@@ -19,5 +19,13 @@ def sign_up():
 def login():
     return render_template('login.html')
 
+@app.route('/discover')
+def discover():
+    return render_template('discover.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
 if __name__== '__main__':
     app.run('0.0.0.0',port=5000, debug=True)
